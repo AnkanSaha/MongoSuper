c204bbf632c37d06f4b64eadc6c67ed70253e707
Ankan Saha <ankansahadaluabari@gmail.com>
2023-06-12 16:36:54 +0530

    Added: Added Find Method

---
+++ src/Bin/Model/CreateModel.ts
@@ -0,0 +1,25 @@
+import { model } from "mongoose"; // Import mongoose types
+
+// Global types
+type globe = any;
+type str = string;
+
+/**
+ * This is an asynchronous function that returns a model based on a given data schema and collection
+ * name, or undefined if no collection name is provided.
+ * @param {globe} DataSchema - The DataSchema parameter is a variable of type "globe", which is not a
+ * standard data type in JavaScript. It is likely a custom data type defined elsewhere in the code.
+ * Without more information, it is difficult to determine what this variable represents.
+ * @param {str} [CollectionName] - CollectionName is an optional parameter of type string. It
+ * represents the name of the collection in the database where the data will be stored. If it is not
+ * provided, the function will return undefined.
+ * @returns If `CollectionName` is `undefined`, then `undefined` is being returned. Otherwise, a model
+ * is being returned based on the `DataSchema` and `CollectionName`.
+ */
+export default function name(DataSchema: globe, CollectionName?: str) {
+    if (CollectionName === undefined) {
+        return undefined;
+    } else {
+        return model(CollectionName, DataSchema);
+    }
+} // end of function
--- src/start/Start.ts
+++ src/start/Start.ts
@@ -1,9 +1,11 @@
 import { connect, connection } from "mongoose"; // import the mongoose module
-import connectDB from "./Connect"; // import the Connect function from the Connect.js file
+import connectDB from "../Connection/Connect"; // import the Connect function from the Connect.js file
+import CreateModel from '../Bin/Model/CreateModel'; // import the Create function from the CreateModel.ts file
 
 // global types
 type str = string;
 type bool = boolean;
+type globe = any;
 
 //  class to run on start/* The above class is a TypeScript implementation of a MongoDB connection
 // handler that can connect to a local or cloud server and listen for
@@ -13,8 +15,12 @@ export class Mongo {
     private MongoURL: str; // string value to store the URL of the MongoDB database to connect to
     private Log: bool; // boolean value to check if the logging is enabled or not
     private ConnectionState: str; // string value to check if the connection is to cloud or local
+    private NeverDisconnect: bool; // boolean value to check if the connection is to cloud or local
+    private Schema: globe; // mongoose schema type
+    private models: globe; // mongoose model type
+    private CollectionName?: str; // string value to store the name of the collection
     private connection: typeof connection; // mongoose connection type
-    private Connect: (MongoURL: str) => Promise<void>; // function to connect to the database
+    private InstantConnect: (MongoURL: str) => Promise<void>; // function to connect to the database
 
     /**
      * This is a constructor function that initializes properties for a MongoDB connection, including
@@ -26,15 +32,22 @@ export class Mongo {
      * to enable logging. If set to true, the code will log information about the database connection.
      * If set to false, no logging will occur.
      */
-    constructor(
-        MongoURL: str = "mongodb://localhost:27017/test", // default value is 'mongodb://localhost:27017/test'
-        Log: bool = true // default value is true
-    ) {
-        this.MongoURL = MongoURL; // assign the MongoURL property
-        this.Log = Log; // assign the Log property
+    constructor(Details: {
+        MongoURL: str; // default value is 'mongodb://localhost:27017/test'
+        Log: bool; // default value is true
+        NeverDisconnect: bool; // default value is false
+        Schema: globe;
+        CollectionName: str;
+    }) {
+        this.MongoURL = Details === undefined || Details.MongoURL === undefined ? "mongodb://localhost:27017/test" : Details.MongoURL; // assign the MongoURL property
+        this.Log = Details === undefined || Details.Log === undefined  ? true : Details.Log; // assign the Log property
+        this.NeverDisconnect = Details === undefined || Details.NeverDisconnect === undefined ? false : Details.NeverDisconnect; // assign the NeverDisconnect property
+        this.Schema = Details === undefined || Details.Schema === undefined ? undefined : Details.Schema; // assign the Schema property
+        this.CollectionName = Details === undefined || Details.CollectionName === undefined ? undefined : Details.CollectionName; // assign the Collection property
         this.ConnectionState = "Local"; // assign the ConnectionState property
         this.connection = connection; // assign the connection property
-        this.Connect = connectDB; // assign the Connect property
+        this.InstantConnect = connectDB; // assign the Connect property
+        this.models = CreateModel(this.Schema, this.CollectionName); // assign the models property
     } // end of constructor
 
     /* The `connect()` method is a method of the `Mongo` class that connects to a MongoDB database using
@@ -42,8 +55,8 @@ the `connect()` function from the `mongoose` module. It also checks if the conne
 or local server by checking if the `MongoURL` property includes the string "mongodb+srv". Finally,
 it calls the `listen()` method to listen for events related to the database connection. */
     // method to connect to the database
-    public async SingleConnect() {
-        await this.Connect(this.MongoURL); // connect to the database
+    public async Connect(): Promise<void> {
+        await this.InstantConnect(this.MongoURL); // connect to the database
         if (this.MongoURL.includes("mongodb+srv")) {
             this.ConnectionState = "Cloud";
         } else {
@@ -55,19 +68,11 @@ it calls the `listen()` method to listen for events related to the database conn
                 `MongoDB connected successfully with ${this.ConnectionState} Server`
             );
         } // log the connection status if logging is enabled
-    } // end of SingleConnect method
-    // method to connect to the database and listen for events
-    public async alwaysConnect(): Promise<void> {
-        await this.Connect(this.MongoURL); // connect to the database
-        if (this.MongoURL.includes("mongodb+srv")) {
-            this.ConnectionState = "Cloud";
-        } else {
-            this.ConnectionState = "Local";
-        } // check if the connection is to cloud or local
 
-        //  listen for events
-        this.listen(); // listen for events
-    } // end of alwaysConnect method
+        if (this.NeverDisconnect === true) {
+            this.listen(); // listen for events related to the database connection
+        } // check if this is a never disconnect connection
+    } // end of SingleConnect method
 
     /* The `private listen()` method is a method of the `Mongo` class that listens for events related to
    the MongoDB database connection. It uses the `connection.on()` method from the `mongoose` module
@@ -112,12 +117,28 @@ it calls the `listen()` method to listen for events related to the database conn
 
     // method to disconnect from the database
     public async disconnect(): Promise<void> {
-        this.connection.close(); // disconnect from the database
-
-        if (this.Log) {
+        if (this.NeverDisconnect === false) {
             console.log(
-                `MongoDB Disconnected successfully with ${this.ConnectionState} Server`
+                "This is not a never disconnect connection, to disconnect use set NeverDisconnect to false"
             );
-        } // log the connection status if logging is enabled
+            return;
+        } // check if this is a never disconnect connection
+        else {
+            this.connection.close(); // disconnect from the database
+            if (this.Log) {
+                console.log(
+                    `MongoDB Disconnected successfully with ${this.ConnectionState} Server`
+                );
+            } // log the connection status if logging is enabled
+        }
     } // end of disconnect method
+
+    // method to find a document in the database
+    public async findOne(Filter:{Where:{}}): Promise<globe> {
+        if(Filter === undefined || Filter.Where === undefined){
+            Filter.Where = {};
+        }
+        const result :globe[] = await this.models.find({$and:[Filter.Where]}); // find the document
+        return result[0]; // return the result
+    } // end of find method
 } // end of alwaysRun class
